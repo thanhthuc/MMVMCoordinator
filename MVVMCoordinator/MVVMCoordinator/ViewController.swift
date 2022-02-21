cd8bf080589ac5d2ad5a53bac6ddae7241fa8176
Thanh Thuc <thanhthuc.ng@samsung.com>
2022-02-21 09:55:42 +0700

    Init MVVM Coordinator

---
+++ MVVMCoordinator/MVVMCoordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMCoordinator
+//
+//  Created by Nguyen Thanh Thuc on 21/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
